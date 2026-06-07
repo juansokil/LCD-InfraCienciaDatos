acc7e5803f684266afd7973bd1952e9f98540c09
Sofía Treyer <streyer@estudiantes.unsam.edu.ar>
2026-06-07 20:00:25 -0300

    Esquema Silver: init.sql & Docker

--- TpFinal/grupos/G03/init.sql
+++ TpFinal/grupos/G03/init.sql
@@ -10,3 +10,34 @@ CREATE TABLE IF NOT EXISTS bronze.raw_weather_data (
     raw_json JSONB NOT NULL,                 -- Guardamos la respuesta completa de la API
     tiempo_extraccion TIMESTAMP NOT NULL    -- Momento exacto en que Airflow hizo la consulta
 );
+
+-- Crear la tabla de datos limpios (Silver)
+
+-- Clima actual
+CREATE TABLE IF NOT EXISTS silver.weather_current (
+    id SERIAL PRIMARY KEY,
+    ciudad VARCHAR(100) NOT NULL, 
+    latitude FLOAT,
+    longitude FLOAT,
+    time TIMESTAMP NOT NULL, -- Fecha de la medición,
+    temperature FLOAT NOT NULL,
+    windspeed FLOAT,
+    winddirection FLOAT,
+    is_day INTEGER,
+    weather_current INTEGER, -- Código meteorológico actual
+    timezone VARCHAR(100) NOT NULL,
+    fecha_procesamiento TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+-- Pronóstico futuro (por default 7 días)
+CREATE TABLE IF NOT EXISTS silver.weather_forecast (
+    id SERIAL PRIMARY KEY,
+    ciudad VARCHAR(100) NOT NULL,
+    fecha_pronostico DATE NOT NULL,
+    temp_min FLOAT NOT NULL,
+    temp_max FLOAT NOT NULL,
+    prob_lluvia FLOAT,
+    weather_forecast INTEGER, -- Código meteorológico pronosticado
+    fecha_procesamiento TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
