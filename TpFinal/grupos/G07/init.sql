bc596b9153f03bebccc987df95abc6d3308f107c
Gastón Ricardo Rossi <gastonricardorossi@gmail.com>
2026-06-13 09:39:10 -0300

    init.sql

--- TpFinal/grupos/G07/init.sql
+++ TpFinal/grupos/G07/init.sql
@@ -1,22 +1,7 @@
--- Creamos el esquema si no existe
-CREATE SCHEMA IF NOT EXISTS silver;
+-- 1. Esquema Origen (Donde caen los datos crudos del sistema externo)
+CREATE SCHEMA IF NOT EXISTS "InfraCienciaDatos";
 
--- Dimensión Estaciones (Datos que no cambian a cada minuto)
-CREATE TABLE IF NOT EXISTS silver.dim_stations (
-    station_id VARCHAR(255) PRIMARY KEY,
-    network_id VARCHAR(255),
-    name VARCHAR(255),
-    latitude NUMERIC,
-    longitude NUMERIC,
-    last_updated TIMESTAMP
-);
-
--- Tabla de Hechos (El historial de las bicicletas)
-CREATE TABLE IF NOT EXISTS silver.fact_station_status (
-    status_id SERIAL PRIMARY KEY,
-    station_id VARCHAR(255),
-    free_bikes INT,
-    empty_slots INT,
-    timestamp_api TIMESTAMP,
-    timestamp_ingesta TIMESTAMP
-);
+-- 2. Esquemas de Arquitectura Medallón (Procesamiento ELT)
+CREATE SCHEMA IF NOT EXISTS "bronze";
+CREATE SCHEMA IF NOT EXISTS "silver";
+CREATE SCHEMA IF NOT EXISTS "gold";
